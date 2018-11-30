1bc0e97923bd3ef201ad3c216756e4f9c04a45fd
rmonfort1990 <rmonfort1990@gmail.com>
2018-11-30 13:22:37 -0300

    medium grafica

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -6,17 +6,19 @@ router.get('/', function(req, res) {
   const openissues = issueStore.getAllOpen();
   const openIssuesCount = openissues.length;
   let highIssuesCount = 0;
+  let mediumIssuesCount = 0;
   if (openIssuesCount > 0)
   {
     highIssuesCount = openissues.filter(issue => issue.severity === 'high').length / openIssuesCount;
+    mediumIssuesCount = openissues.filter(issue => issue.severity === 'medium').length / openIssuesCount;
   }
   else
   {
     highIssuesCount = 0;
-  }
-  
+    mediumIssuesCount = 0;
+  }  
 
-  res.render('dashboard', {openIssuesCount, highIssuesCount});
+  res.render('dashboard', {openIssuesCount, highIssuesCount, mediumIssuesCount});
 });
 
 module.exports = router;
